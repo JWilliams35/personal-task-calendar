f1de003eb996a0e30d8bf60e97c4773095531acb
Jovan Williams <jovanwilliams1995@gmail.com>
2018-01-29 19:43:39 -0500

    started working on tabs

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  const tabs = [{
+    name: 'Personal',
+    content: 'placeholder tab 1'
+  },{
+    name: 'Education',
+    content: 'placeholder tab2'
+  },{
+    name: 'Social',
+    content: 'Content for tab 3'
+  }];
 }
 
 export default App;
