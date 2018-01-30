7848f59b3bb4a421d96de3c68e262e792e0f5b6c
Jovan Williams <jovanwilliams1995@gmail.com>
2018-01-29 20:11:51 -0500

    added clickable tabs (first try)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  const tabs = [{
+const tabs = [{
     name: 'Personal',
     content: 'placeholder tab 1'
   },{
@@ -13,6 +12,67 @@ class App extends Component {
     name: 'Social',
     content: 'Content for tab 3'
   }];
+
+const App = (props) => {
+  return (
+      <Tabs selected = {props.firstSelect || 0}>
+      {props.tabs.map(tab => 
+        <Pane label={tab.name}>{tab.content}</Pane>)
+    }
+    </Tabs>
+    );
+}
+
+const Pane = (props) => {
+  return <div>{props.childern}</div>;
+}
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {selected: this.props.selected};
+  }
+
+  _renderTitles(){
+    function labels(child, idx){
+      let activeClass = (this.state.selected === idx ? 'is-active': '');
+      return (
+        <li role="tab" key={idx} aria-controls={`panel${idx}`}>
+          <a className={activeClass} onClick={this.onClick.bond(this, idx)} href="#">
+            {child.props.label}
+          </a>
+        </li>
+      );
+    }
+
+    return(
+      <ul className="tabs_labels" role="tablist">
+        {this.props.children.map(labels.bind(this))}
+      </ul>
+    );
+  }
+
+  onClick(index, event){
+    event.preventDefault();
+    this.setState({
+      selectted: index
+    });
+  }
+
+  render(){
+    return(
+        <div className="tabs">
+          {this._renderTitles()}
+
+          <div className="tabs_content">
+            {this.props.children[this.state.selected]}
+          </div>
+        </div>
+      );
+  }
 }
 
 export default App;
+
+//ReactDOM.render(<App tabs={tabs} firstSelect={1} />, document.getElementById('app'));
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Starter from './components/test';
+//import Starter from './components/test';
 import registerServiceWorker from './registerServiceWorker';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Starter />, document.getElementById('root'));
+//ReactDOM.render(<Starter />, document.getElementById('root'));
+ReactDOM.render(<App tabs={tabs} firstSelect={1} />, document.getElementById('app'));
 registerServiceWorker();
