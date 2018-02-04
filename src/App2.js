0da78de869fed3f378112b33375042ea7772e43c
Jovan Williams <jovanwilliams1995@gmail.com>
2018-02-03 20:43:17 -0500

    overhaul listing function and removed unused files, WIP

--- src/App2.js
+++ src/App2.js
@@ -1,37 +1,16 @@
-import React from 'react';
-import Tabs from './components/Tabs.js';
-import Starter from './components/test.js';
+import React, {Component} from 'react';
+import './App.css';
+import Header from './component/header.js';
 
-const styles = {
-  fontFamily: 'sans-serif',
-};
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {active: 'aTab'};
-  }
+class App extends Component {
   render() {
-    const content = {
-      aTab: <Starter/>,
-      bTab: <Starter />,
-      cTab: <Starter />,
-    };
-    return (
-      <div style={styles}>
-        <h1>Tabs Example</h1>
-        <Tabs
-          active={this.state.active}
-          onChange={active => this.setState({active})}
-        >
-          <div key="aTab">A Title</div>
-          <div key="bTab">B Very Long Title</div>
-          <div key="cTab">{this.state.active === 'bTab' ? 'Title' : 'Very Long Title'}</div>
-        </Tabs>
-        <h2>Content</h2>
-        <p>{content[this.state.active]}</p>
+    return(
+      <div className="App">
+        <div className="todo-wrapper">
+          <Header />
+        </div>
       </div>
-    );
+    )
   }
-};
+}
 export default App;
