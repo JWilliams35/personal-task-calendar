fac9336c3b69b335faa8a31875466cdf416b97c2
Jovan Williams <jovanwilliams1995@gmail.com>
2018-03-07 16:51:13 -0500

    replaced place holders with some functionality

--- src/App2.js
+++ src/App2.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react';
 import './css/App.css';
 import Header from './component/header.js';
-import TodoInput from './component/todoInput';
+import TodoInput from './component/Todo.js';
 
 class App extends Component {
+  constructor{props}{
+    super(props);
+
+    this.state= {
+      todos:{
+        {id: 0, text: "Continue working on this"},
+        {id: 1, text: "Buy food"},
+        {id: 2, text: "Go to sleep at a proper time"}
+      },
+      nextId: 3 
+    }
+
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  addTodo(todoText){
+    console.log("Task added: ",todoText);
+  }
+
+  removeTodo(id){
+    console.log("Task removed: ", id);
+  }
+  
   render() {
     return(
       <div className="App">
         <div className="todo-wrapper">
           <Header />
-          <TodoInput />
+          <TodoInput todoText="" addTodo={this.addTodo}/>
         </div>
       </div>
     )
